8d324506dbbd765fe079de796895e1532c783450
nbthales <nbthales@gmail.com>
2023-06-09 15:59:40 -0300

    ensure HttpAdapter throws BadRequestError on 400

--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -3,6 +3,7 @@ import 'package:http/http.dart';
 import 'package:mockito/mockito.dart';
 import 'package:flutter_test/flutter_test.dart';
 
+import 'package:clean_flutter_app/data/http/http.dart';
 import 'package:clean_flutter_app/infra/http/http.dart';
 
 class ClientSpy extends Mock implements Client {}
@@ -82,6 +83,14 @@ void main() {
       final response = await sut.request(url: url, method: 'post');
 
       expect(response, null);
+    });
+
+    test('Should return BadRequestError if post returns 400', () async {
+      mockResponse(400);
+
+      final future = sut.request(url: url, method: 'post');
+
+      expect(future, throwsA(HttpError.badRequest));
     });
   });
 }
