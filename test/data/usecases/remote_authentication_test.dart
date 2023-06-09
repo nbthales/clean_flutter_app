ec78a1b2a14d70be631846bab38a383ff03064e7
nbthales <nbthales@gmail.com>
2023-06-09 09:04:22 -0300

    ensure RemoteAuthentication throws UnexpectedError on 200 with invalid data

--- test/data/usecases/remote_authentication_test.dart
+++ test/data/usecases/remote_authentication_test.dart
@@ -104,4 +104,18 @@ void main() {
 
     expect(account.token, acessToken);
   });
+
+  test(
+      'Should throw UnexpectedError if HttpClient return 200 with invalid data',
+      () async {
+    when(httpClient.request(
+      url: anyNamed('url'),
+      method: anyNamed('method'),
+      body: anyNamed('body'),
+    )).thenAnswer((_) async => {'invalid_key': 'invalid_value'});
+
+    final future = sut.auth(params);
+
+    expect(future, throwsA(DomainError.unexpected));
+  });
 }
